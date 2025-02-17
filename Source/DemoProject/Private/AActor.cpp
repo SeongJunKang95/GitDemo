7df05046bc6999132f876f13b22ea4957659aa7e
SeongJunKang95 <skyrnt777@gmail.com>
2025-02-17 19:20:16 +0900

    AA액터 헤더, cpp 파일 수정

--- Source/DemoProject/Private/AActor.cpp
+++ Source/DemoProject/Private/AActor.cpp
@@ -1,27 +1,11 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "AActor.h"
 
-// Sets default values
+
 AAActor::AAActor()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+ 	
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
-// Called when the game starts or when spawned
-void AAActor::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void AAActor::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
 
